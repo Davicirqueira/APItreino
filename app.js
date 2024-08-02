5743b144c1768c268ca3b18eddfee4976952eb16
davizin <daviforever17@gmail.com>
2024-08-02 15:08:18 -0300

    APIsoma

---
+++ app.js
@@ -0,0 +1,23 @@
+import express from 'express';
+
+const servidor = express()
+
+servidor.use(express.json());
+
+servidor.get('/calculadora/somar/:n1/:n2', (req, resp) => {
+
+    let n1 = Number(req.params.n1)
+
+    let n2 = Number(req.params.n2)
+
+    let soma = n1 + n2
+
+    resp.send({
+
+        soma: soma
+
+    })
+
+})
+
+servidor.listen(5005, () => console.log('------> API subiu com sucesso!!'))
